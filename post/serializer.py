8bd1069d2cdee2874c8951c65a1bd4c011d39268
badushaebrahim <bbadusha13@gmail.com>
2022-08-11 16:03:07 +0530

    working state with out many many

--- post/serializer.py
+++ post/serializer.py
@@ -2,6 +2,13 @@ from pyexpat import model
 from rest_framework import serializers
 from .models import posts,comment
 
+class commentserializer(serializers.ModelSerializer):
+    class Meta:
+        model = comment
+        fields = '__all__'
+
+
+
 class postserializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField(read_only=True)
     class Meta:
@@ -15,8 +22,11 @@ class postserializer(serializers.ModelSerializer):
         ]
         depth =2
     def get_comments(self, obj):
-        qs = comment.objects.filter(ofpost=obj).count()
-        return qs
+        qs = comment.objects.all().filter(ofpost=obj)
+        print(type(qs))
+        qs2 = commentserializer(qs,many=True)
+        print(qs2.data)
+        return qs2.data
 
 
 
