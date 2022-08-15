ef30aa42ac21994f4019dd6efd98cb0596ab1d7d
badushaebrahim <bbadusha13@gmail.com>
2022-08-15 18:22:40 +0530

    mailing at a good condition burt takeing toomuch timein celary

--- post/task.py
+++ post/task.py
@@ -3,17 +3,22 @@ from django.contrib.auth import get_user_model
 from celery import shared_task
 from django.core.mail import send_mail
 from blog22 import settings
-from django.utils import timezone
-from datetime import timedelta
+# from django.utils import timezone
+# from datetime import timedelta
 
 
 @shared_task
-def sent_mail(self,msg,email):
+def sent_mail2(msg,email):
         send_mail(
-            subject = "test mail",
+            subject = "Alert  mail",
             message=msg,
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[email],
             fail_silently=True,
         )
-        return "Done"
+        
+        
+@shared_task
+def test(msg,email):
+    print(msg,email)
+    return "Done"
--- post/views.py
+++ post/views.py
@@ -12,7 +12,7 @@ from account.models import CustomUser
 from .models import comment, posts
 from .serializer import commentserializer, postserializer,createpostserializer,postserializer_byid,commentgetserialiser
 from account.serializer import loginserializer
-from .task import send_mail
+from .task import sent_mail2,test
 # Create your views here.
 
 
@@ -190,8 +190,11 @@ def make_comment(request):
                 if str(request.user) == ser.data["first_name"]:
                     if newcommentserial.is_valid():
                         newcommentserial.save()
-                        send_mail("user commnted",ser.data["email"])
+                        # sent_mail2("user comented on your post ",ser.data["email"])
+                        test("user comented on your post ",ser.data["email"])
+                        # if(res == "Done"):
                         return Response(newcommentserial.data,status=status.HTTP_200_OK)
+                        
                     # print(newcommentserial.error_messages)
 
                 return Response( newcommentserial.error_messages, status=status.HTTP_400_BAD_REQUEST)
