964825f9f9d993531ac26e6941b27b0b5eb16908
badushaebrahim <bbadusha13@gmail.com>
2022-08-17 09:38:45 +0530

    updated security

--- blog22/settings.py
+++ blog22/settings.py
@@ -196,7 +196,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='badushaebrahim62@gmail.com'
-EMAIL_HOST_PASSWORD = "cahixlvazhupwncjez"
+EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASSWORD")
 DEFAULT_FROM_EMAIL = 'Celery <badushaebrahim62@gmail.com>'
 
 
