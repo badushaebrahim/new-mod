34150f3b8fe81c458a4f2b4d6d27f82e6f9b6bfb
badushaebrahim <bbadusha13@gmail.com>
2022-08-18 13:45:23 +0530

    doc string

--- post/models.py
+++ post/models.py
@@ -3,7 +3,7 @@ from account.models import CustomUser
 # Create your models here.
 
 class posts(models.Model):
-    '''post moidel'''
+    '''post model'''
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=10000)
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
@@ -17,7 +17,7 @@ class comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     ofpost = models.ForeignKey(posts,on_delete=models.CASCADE)
     
-    
+    '''this is propertuy used to list comment'''
     @property
     def comments(self):
         instance = self
