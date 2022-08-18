9e5bd7f412b63a64c819677392c0ad9b6ab5b3c0
badushaebrahim <bbadusha13@gmail.com>
2022-08-18 10:58:11 +0530

    task pep8 complete

--- post/task.py
+++ post/task.py
@@ -1,10 +1,13 @@
+''' imports of celary shaered task, send mail function and setting sto get mail'''
 from celery import shared_task
 from django.core.mail import send_mail
 from blog22 import settings
 
 
+
 @shared_task
 def sent_mail2(msg, email):
+    '''task to sent mail when coment is made'''
     send_mail(
         subject="Alert  mail",
         message=msg,
@@ -15,7 +18,10 @@ def sent_mail2(msg, email):
     return f"mail sent{email}"
 
 
+
+
 @shared_task
 def test(msg, email):
+    '''task to test celary'''
     print(msg, email)
     return "Done"
